74ab6b5322b09516c29e0c205b5c1e3e4e2dcda7
GOPIKRISHNAN R <gopikrishnan1301@gmail.com>
2026-08-14 10:10:34 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int val : nums) set.add(val);
+        return !(set.size() == nums.length);
+    }
+}
